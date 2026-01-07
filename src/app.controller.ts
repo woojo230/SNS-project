4a00d50a1bcefff2cdb29bb438f90382cef03950
woojo230 <woojunghyck@naver.com>
2026-01-07 17:06:15 +0900

    feat: Post json 반환

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+interface Post {
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getPost(): Post {
+    return {
+      author: 'Tyron Woo',
+      title: 'COCO',
+      content: 'I got the coco',
+      likeCount: 38,
+      commentCount: 40,
+    };
   }
 }
