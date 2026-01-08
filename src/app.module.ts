532990ff38ca5d1bc9202a0839a6898e1b78a9d2
woojo230 <woojunghyck@naver.com>
2026-01-08 19:35:51 +0900

    setting(postgres): typeORM, pg 라이브러리 설치 및  postgres 연동

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    PostsModule,
+    TypeOrmModule.forRoot({
+      // database type
+      type: 'postgres',
+      host: '127.0.0.1',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      entities: [],
+      synchronize: true, // 배포시 false 로 설정
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
