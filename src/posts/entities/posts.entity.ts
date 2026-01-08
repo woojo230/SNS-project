8a597fdba30d9c0eb6ce806a8c10c936f30fcef7
woojo230 <woojunghyck@naver.com>
2026-01-08 19:45:54 +0900

    feat(postgres): TypeORM을 활용한 PostgreSQL 연동 및 초기 Table 생성

---
+++ src/posts/entities/posts.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class PostModel {
+  @PrimaryGeneratedColumn() // 유일무이한 값
+  id: number;
+
+  @Column()
+  author: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  content: string;
+
+  @Column()
+  likeCount: number;
+
+  @Column()
+  commentCount: number;
+}
