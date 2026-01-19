d39ff8456b23dbce6986daf3fbd3d8ce1b084eb7
woojo230 <woojunghyck@naver.com>
2026-01-19 21:43:30 +0900

    feat(post): globalpipe implicitConversion 설정 및 Type 어노테이션을 활용하여 형변환

--- src/posts/dto/paginate-post.dto.ts
+++ src/posts/dto/paginate-post.dto.ts
@@ -1,8 +1,10 @@
+import { Type } from 'class-transformer';
 import { IsIn, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class PaginatePostDto {
   // 이전 마지막 데이터의 ID값
   // 이 프로퍼티에 입력된 ID보다 높은 ID 부터 값을 가져오기
+  @Type(() => Number)
   @IsNumber()
   @IsOptional()
   where__id_more_than?: number;
