596ad44034aeffadad8e0293a6d8826bebe8b5d5
woojo230 <woojunghyck@naver.com>
2026-01-20 23:52:58 +0900

    feat(file-upload): static file serving 옵션 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,10 +17,16 @@ import {
   ENV_DB_PORT_KEY,
   ENV_DB_USERNAME_KEY,
 } from './common/const/env-keys.const';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { PUBLIC_FOLDER_PATH } from './common/const/path.const';
 
 @Module({
   imports: [
     PostsModule,
+    ServeStaticModule.forRoot({
+      rootPath: PUBLIC_FOLDER_PATH,
+      serveRoot: '/public',
+    }),
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true,
