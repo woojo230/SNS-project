741ea306216aa0403e96c9ca880c6ba8a953900d
woojo230 <woojunghyck@naver.com>
2026-01-21 21:39:07 +0900

    refactor(file-upload): 선 업로드 방식으로 수정1

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,8 +1,46 @@
-import { Module } from '@nestjs/common';
+import { BadRequestException, Module } from '@nestjs/common';
 import { CommonService } from './common.service';
 import { CommonController } from './common.controller';
+import { MulterModule } from '@nestjs/platform-express';
+import { extname } from 'path';
+import multer from 'multer';
+import { POST_IMAGE_PATH, TEMP_FOLDER_PATH } from './const/path.const';
+import { v4 as uuid } from 'uuid';
+import { AuthModule } from 'src/auth/auth.module';
+import { UsersModule } from 'src/users/users.module';
+import { AuthService } from 'src/auth/auth.service';
+import { UsersService } from 'src/users/users.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    AuthModule,
+    UsersModule,
+    JwtModule.register({}),
+    MulterModule.register({
+      limits: {
+        fileSize: 1000000, //10MB
+      },
+      fileFilter: (req, file, cb) => {
+        const ext = extname(file.originalname);
+        if (ext !== '.jpg' && ext !== '.jpeg' && ext !== '.png') {
+          return cb(
+            new BadRequestException('jpg/jpeg/png 파일만 업로드 가능합니다'),
+            false, // 다운로드 불가 옵션
+          );
+        }
+        return cb(null, true);
+      },
+      storage: multer.diskStorage({
+        destination: function (req, res, cb) {
+          cb(null, TEMP_FOLDER_PATH);
+        },
+        filename: function (req, file, cb) {
+          cb(null, `${uuid()}${extname(file.originalname)}`);
+        },
+      }),
+    }),
+  ],
   controllers: [CommonController],
   providers: [CommonService],
 })
--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -17,29 +17,6 @@ import { v4 as uuid } from 'uuid';
   imports: [
     JwtModule.register({}),
     TypeOrmModule.forFeature([PostsModel, UsersModel]),
-    MulterModule.register({
-      limits: {
-        fileSize: 1000000, //10MB
-      },
-      fileFilter: (req, file, cb) => {
-        const ext = extname(file.originalname);
-        if (ext !== '.jpg' && ext !== '.jpeg' && ext !== '.png') {
-          return cb(
-            new BadRequestException('jpg/jpeg/png 파일만 업로드 가능합니다'),
-            false, // 다운로드 불가 옵션
-          );
-        }
-        return cb(null, true);
-      },
-      storage: multer.diskStorage({
-        destination: function (req, res, cb) {
-          cb(null, POST_IMAGE_PATH);
-        },
-        filename: function (req, file, cb) {
-          cb(null, `${uuid()}${extname(file.originalname)}`);
-        },
-      }),
-    }),
   ],
   controllers: [PostsController],
   providers: [PostsService, AuthService, UsersService],
